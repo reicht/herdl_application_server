3edc381b95b71466b49cf4eca8ef4185143a90ad
Jordan Corley <rainwolfj@gmail.com>
2016-04-09 17:39:44 -0500

    Slight modification to options handling, fiddling with routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  match 'api/entries', to: 'api/entries#index', via: [:options]
-  match 'api/entries/:id/votes', to: 'api/votes#index', via: [:options]
+  match 'api/entries', to: 'api/entries#create', via: [:options]
+  match 'api/entries/:id/votes', to: 'api/votes#create', via: [:options]
 
   namespace :api do
     resources :entries do
