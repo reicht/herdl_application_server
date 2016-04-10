b1120fd9f7a9735d92dd8e41fca8b1eec8d19d2f
Jordan Corley <rainwolfj@gmail.com>
2016-04-10 17:57:33 -0500

    Now creates a base upvote so that every post has at least one vote.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,12 +1,18 @@
 class Entry < ActiveRecord::Base
   has_many :votes, dependent: :destroy
 
+  after_create :create_base_vote
+
   def up_votes
     self.votes.where('up_vote > 0').count
   end
 
   def down_votes
     self.votes.where('down_vote > 0').count
+  end
+
+  def create_base_vote
+    Vote.create!(entry_id: self.id, up_vote: 1, down_vote: 0)
   end
 
 end
